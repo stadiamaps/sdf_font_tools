86a99307f224d4875f558f25624f9c1451233a9a
Ian Wagner <ian.wagner@stadiamaps.com>
2020-02-12 20:47:04 +0900

    Upgrade to futures 0.3 and tokio 0.2

--- src/lib.rs
+++ src/lib.rs
@@ -9,77 +9,71 @@
 //! ## References
 //!   * https://github.com/mapbox/glyph-pbf-composite
 //!   * https://github.com/klokantech/tileserver-gl/blob/master/src/utils.js
-extern crate futures;
-extern crate protobuf;
-extern crate tokio;
 
-use futures::Future;
+#![deny(warnings)]
 
 use std::collections::HashSet;
 use std::path::Path;
 
+use futures::future::try_join_all;
+
 use tokio::fs::File;
-use tokio::io;
+use tokio::io::AsyncReadExt;
 
 mod glyphs;
 
-type GlyphFuture = Box<Future<Item = glyphs::glyphs, Error = protobuf::ProtobufError> + Send>;
+type GlyphResult = Result<glyphs::glyphs, protobuf::ProtobufError>;
 
 /// Generates a single combined font stack for the set of fonts provided.
 ///
 /// See the documentation for `combine_glyphs` for further details.
 /// Unlike `combine_glyphs`, the result of this method will always contain a `glyphs` message,
 /// even if the loaded range is empty for a given font.
-pub fn get_font_stack(
+pub async fn get_font_stack(
     font_path: &Path,
     font_names: Vec<String>,
     start: u32,
     end: u32,
-) -> GlyphFuture {
+) -> GlyphResult {
     // Load fonts (futures)
-    let load_futures: Vec<GlyphFuture> = font_names
-        .iter()
-        .map(|font| -> GlyphFuture { load_glyphs(font_path, font, start, end) })
-        .collect();
+    let mut load_futures: Vec<_> = vec![];
+    for font in font_names.iter() {
+        load_futures.push(load_glyphs(font_path, font, start, end));
+    }
 
     // Combine all the glyphs into a single instance, using the ordering to determine priority.
-    let combinator = futures::future::join_all(load_futures);
-    Box::new(combinator.map(move |data| -> glyphs::glyphs {
-        if let Some(result) = combine_glyphs(data) {
-            result
-        } else {
-            // Construct an empty message manually if the range is not covered
-            let mut result = glyphs::glyphs::new();
+    match try_join_all(load_futures).await {
+        Ok(data) => {
+            if let Some(result) = combine_glyphs(data) {
+                Ok(result)
+            } else {
+                // Construct an empty message manually if the range is not covered
+                let mut result = glyphs::glyphs::new();
 
-            let mut stack = glyphs::fontstack::new();
-            stack.set_name(font_names.join(", "));
-            stack.set_range(format!("{}-{}", start, end));
+                let mut stack = glyphs::fontstack::new();
+                stack.set_name(font_names.join(", "));
+                stack.set_range(format!("{}-{}", start, end));
 
-            result.mut_stacks().push(stack);
-            result
+                result.mut_stacks().push(stack);
+                Ok(result)
+            }
         }
-    }))
+        Err(e) => Err(e),
+    }
 }
 
 /// Loads a single font PBF slice from disk.
 ///
 /// Fonts are assumed to be stored in <font_path>/<font_name>/<start>-<end>.pbf.
-pub fn load_glyphs(font_path: &Path, font_name: &str, start: u32, end: u32) -> GlyphFuture {
+pub async fn load_glyphs(font_path: &Path, font_name: &str, start: u32, end: u32) -> GlyphResult {
     let full_path = font_path
         .join(font_name)
         .join(format!("{}-{}.pbf", start, end));
-    let fut = File::open(full_path)
-        .or_else(|e| futures::future::err(protobuf::ProtobufError::from(e)))
-        .and_then(|file| {
-            io::read_to_end(file, Vec::new())
-                .or_else(|e| {
-                    futures::future::err(protobuf::ProtobufError::from(e))
-                })
-                .and_then(|(_file, data)| -> protobuf::ProtobufResult<glyphs::glyphs> {
-                    protobuf::parse_from_bytes(&data)
-                })
-        });
-    Box::new(fut)
+    let mut file = File::open(full_path).await?;
+    let mut buf = Vec::new();
+
+    file.read_to_end(&mut buf).await?;
+    protobuf::parse_from_bytes(&buf)
 }
 
 /// Combines a list of SDF font glyphs into a single glyphs message.
@@ -120,5 +114,5 @@ pub fn combine_glyphs(data: Vec<glyphs::glyphs>) -> Option<glyphs::glyphs> {
 
     result.mut_stacks().push(combined_stack);
 
-    return Some(result);
+    Some(result)
 }
--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,95 +1,69 @@
-extern crate futures;
-extern crate tokio;
-
 extern crate pbf_font_tools;
 
-use futures::future::Future;
-
-use std::path::Path;
-use std::sync::mpsc;
+use futures::future::join;
 use std::collections::HashMap;
+use std::path::Path;
 
-#[test]
-fn test_load_glyphs() {
+#[tokio::test]
+async fn test_load_glyphs() {
     let font_path = Path::new("tests").join("glyphs");
-    let (tx, rx) = mpsc::channel();
+    let font_name = "SeoulNamsan L";
+    let result = pbf_font_tools::load_glyphs(font_path.as_path(), font_name, 0, 255).await;
 
-    let glyphs_fut = pbf_font_tools::load_glyphs(font_path.as_path(), "SeoulNamsan L", 0, 255)
-        .then(move |result| {
-            match result {
-                Ok(glyphs) => {
-                    // Make sure we have a font stack, and that it has the expected name
-                    // and glyph count
-                    let stack = &glyphs.get_stacks()[0];
-                    let glyph_count = stack.get_glyphs().len();
-                    let success = stack.get_name() == "SeoulNamsan L" && glyph_count == 170;
-
-                    tx.send(success).expect("Failed to send message");
-                    futures::future::ok(())
-                }
-                Err(_) => {
-                    tx.send(false).expect("Failed to send message");
-                    futures::future::err(())
-                }
-            }
-        });
-    tokio::run(glyphs_fut);
-
-    let result = rx.recv().expect("Failed to receive message");
-    assert!(result);
+    match result {
+        Ok(glyphs) => {
+            let stack = &glyphs.get_stacks()[0];
+            let glyph_count = stack.get_glyphs().len();
+            assert_eq!(stack.get_name(), font_name);
+            assert_eq!(glyph_count, 170);
+        }
+        Err(e) => panic!("Encountered error {:#?}.", e),
+    }
 }
 
-#[test]
-fn test_get_font_stack() {
+#[tokio::test]
+async fn test_get_font_stack() {
     let font_path = Path::new("tests").join("glyphs");
     let font_names = vec![
         String::from("SeoulNamsan L"),
         String::from("Open Sans Light"),
     ];
-    let (tx, rx) = mpsc::channel();
 
     let namsan_font_future =
         pbf_font_tools::load_glyphs(font_path.as_path(), "SeoulNamsan L", 0, 255);
 
     let font_stack_fut = pbf_font_tools::get_font_stack(font_path.as_path(), font_names, 0, 255);
 
-    let combinator = namsan_font_future.join(font_stack_fut);
+    let result = join(namsan_font_future, font_stack_fut).await;
 
-    tokio::run(combinator.then(move |result| {
-        match result {
-            Ok((namsan_glyphs, combined_glyphs)) => {
-                // Make sure we have a font stack, and that it has the expected name
-                // and glyph count.
-                let stack = &combined_glyphs.get_stacks()[0];
-                let glyph_count = stack.get_glyphs().len();
+    match result {
+        (Ok(namsan_glyphs), Ok(combined_glyphs)) => {
+            // Make sure we have a font stack, and that it has the expected name
+            // and glyph count.
+            let stack = &combined_glyphs.get_stacks()[0];
+            let glyph_count = stack.get_glyphs().len();
 
-                let mut success =
-                    stack.get_name() == "SeoulNamsan L, Open Sans Light" && glyph_count == 228;
+            assert_eq!(stack.get_name(), "SeoulNamsan L, Open Sans Light");
+            assert_eq!(glyph_count, 228);
 
-                let namsan_stack = &namsan_glyphs.get_stacks()[0];
-                let namsan_mapping: HashMap<u32, Vec<u8>> = namsan_stack.get_glyphs().iter().map(|x| {
-                    (x.get_id(), Vec::from(x.get_bitmap()))
-                }).collect();
+            let namsan_stack = &namsan_glyphs.get_stacks()[0];
+            let namsan_mapping: HashMap<u32, Vec<u8>> = namsan_stack
+                .get_glyphs()
+                .iter()
+                .map(|x| (x.get_id(), Vec::from(x.get_bitmap())))
+                .collect();
 
-                // Make sure the Namsan font glyphs took precedence over the Open Sans ones.
-                for glyph in stack.get_glyphs() {
-                    if let Some(namsan_glyph) = namsan_mapping.get(&glyph.get_id()) {
-                        if !namsan_glyph.eq(&Vec::from(glyph.get_bitmap())) {
-                            success = false;
-                        }
+            // Make sure the Namsan font glyphs took precedence over the Open Sans ones.
+            for glyph in stack.get_glyphs() {
+                if let Some(namsan_glyph) = namsan_mapping.get(&glyph.get_id()) {
+                    if !namsan_glyph.eq(&Vec::from(glyph.get_bitmap())) {
+                        panic!("Encountered glyph where Namsan was overwritten by Open Sans.");
                     }
                 }
-
-                tx.send(success).expect("Failed to send message");
-                futures::future::ok(())
-            }
-            Err(_) => {
-                tx.send(false).expect("Failed to send message");
-                futures::future::err(())
             }
         }
-    }));
-
-    let result = rx.recv().expect("Failed to receive message");
-    assert!(result);
+        _ => {
+            panic!("One of the futures failed to complete successfully.");
+        }
+    }
 }
