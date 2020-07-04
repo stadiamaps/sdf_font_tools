d2c0e0b11ca19a00a923d44bcb9d6c432e58724a
Ian Wagner <ian.wagner@stadiamaps.com>
2020-07-04 15:01:09 +0900

    Improve tests to more accurately sanity check the glyph combination logic

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,6 +1,6 @@
 extern crate pbf_font_tools;
 
-use futures::future::join;
+use futures::future::join3;
 use std::collections::HashMap;
 use std::path::Path;
 
@@ -32,12 +32,15 @@ async fn test_get_font_stack() {
     let namsan_font_future =
         pbf_font_tools::load_glyphs(font_path.as_path(), "SeoulNamsan L", 0, 255);
 
+    let open_sans_font_future =
+        pbf_font_tools::load_glyphs(font_path.as_path(), "Open Sans Light", 0, 255);
+
     let font_stack_fut = pbf_font_tools::get_font_stack(font_path.as_path(), font_names, 0, 255);
 
-    let result = join(namsan_font_future, font_stack_fut).await;
+    let result = join3(namsan_font_future, open_sans_font_future, font_stack_fut).await;
 
     match result {
-        (Ok(namsan_glyphs), Ok(combined_glyphs)) => {
+        (Ok(namsan_glyphs), Ok(open_sans_glyphs), Ok(combined_glyphs)) => {
             // Make sure we have a font stack, and that it has the expected name
             // and glyph count.
             let stack = &combined_glyphs.get_stacks()[0];
@@ -53,14 +56,32 @@ async fn test_get_font_stack() {
                 .map(|x| (x.get_id(), Vec::from(x.get_bitmap())))
                 .collect();
 
+            let open_sans_stack = &open_sans_glyphs.get_stacks()[0];
+            let open_sans_mapping: HashMap<u32, Vec<u8>> = open_sans_stack
+                .get_glyphs()
+                .iter()
+                .map(|x| (x.get_id(), Vec::from(x.get_bitmap())))
+                .collect();
+
+            let mut has_open_sans_glyph = false;
+
             // Make sure the Namsan font glyphs took precedence over the Open Sans ones.
             for glyph in stack.get_glyphs() {
                 if let Some(namsan_glyph) = namsan_mapping.get(&glyph.get_id()) {
                     if !namsan_glyph.eq(&Vec::from(glyph.get_bitmap())) {
                         panic!("Encountered glyph where Namsan was overwritten by Open Sans.");
                     }
+                } else if let Some(_) = open_sans_mapping.get(&glyph.get_id()) {
+                    has_open_sans_glyph = true;
+                } else {
+                    panic!("Uh, where did this glyph come from?");
                 }
             }
+
+            assert!(
+                has_open_sans_glyph,
+                "Should have at least one Open Sans glyph"
+            );
         }
         _ => {
             panic!("One of the futures failed to complete successfully.");
