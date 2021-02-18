6119d8e28713c7d559eb836e42801a5265a7dbf7
Ian Wagner <ian@funktional.ee>
2021-02-19 03:03:46 +0900

    Improve root module docs for crates.io

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
+//! This crate is a Rust implementation of the signed distance field generation techniques
+//! demonstrated by [Valve](https://steamcdn-a.akamaihd.net/apps/valve/2007/SIGGRAPH2007_AlphaTestedMagnification.pdf)
+//! and [Mapbox](https://blog.mapbox.com/drawing-text-with-signed-distance-fields-in-mapbox-gl-b0933af6f817).
+//! The generic interface works with any bitmap, and a high level interface enables easy operation
+//! with FreeType faces when the optional `freetype` feature is enabled.
+//!
+//! The approach taken by this crate is similar to [TinySDF](https://github.com/mapbox/tiny-sdf);
+//! it works from a raster bitmap rather than directly from vector outlines. This keeps the
+//! crate simple and allows it to be used generically with any bitmap. The SDF is calculated
+//! using the same algorithm described in [this paper](http://cs.brown.edu/people/pfelzens/papers/dt-final.pdf)
+//! by Felzenszwalb & Huttenlocher.
+//!
+//! Rather than re-invent the rasterisation process for fonts, this crate relies on FreeType to
+//! generate the bitmap. This is quite fast (we're talking µs/glyph), and the results are
+//! almost always indistinguishable from the more sophisticated vector-based approach of
+//! [sdf-glyph-foundry](https://github.com/mapbox/sdf-glyph-foundry).
+//!
+//! This crate is used by [pbf_font_tools](https://github.com/stadiamaps/pbf_font_tools) to generate
+//! SDF glyphs from any FreeType-readable font. If you're looking for a batch generation tool,
+//! check out [build_pbf_glyphs](https://github.com/stadiamaps/build_pbf_glyphs).
+
 #[cfg(test)]
 mod tests;
 
