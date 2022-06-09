bc9e75dd98d40f9f010f180db07997e37384f9e8
Luke Seelenbinder <me@lukeseelenbinder.com>
2022-06-09 11:19:57 +0200

    Connect vendored protoc to build script.

--- build.rs
+++ build.rs
@@ -1,6 +1,9 @@
+use protoc_bin_vendored::protoc_bin_path;
+
 fn main() {
     protobuf_codegen::Codegen::new()
         .cargo_out_dir("protos")
+        .protoc_path(&protoc_bin_path().unwrap())
         .input("proto/glyphs.proto")
         .include("proto/")
         .run()
