7518ec762d1301bdb91b5ba5228d0765e054a24c
Ian Wagner <ian.wagner@stadiamaps.com>
2025-07-07 13:51:53 +0900

    Try again?

--- pbf_font_tools/build.rs
+++ pbf_font_tools/build.rs
@@ -1,10 +1,14 @@
 use std::io::Result;
 
 fn main() -> Result<()> {
+    // protoc compiled from source
     #[cfg(feature = "protoc-from-src")]
     std::env::set_var("PROTOC", protobuf_src::protoc());
+
+    // Vendored protoc binary
     #[cfg(feature = "protoc-vendored")]
     std::env::set_var("PROTOC", protoc_bin_vendored::protoc_bin_path());
+
     prost_build::compile_protos(&["proto/glyphs.proto"], &["proto/"])?;
     Ok(())
 }
