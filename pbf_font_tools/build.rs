f3b593f29292079fcd1205d980cc71aae3829f7b
Ian Wagner <ian.wagner@stadiamaps.com>
2026-03-13 15:36:58 +0900

    cargo fmt

--- pbf_font_tools/build.rs
+++ pbf_font_tools/build.rs
@@ -18,12 +18,11 @@ fn main() -> Result<()> {
     // Vendored protoc binary
     #[cfg(feature = "protoc-vendored")]
     if let Ok(vendored_protoc_path) = protoc_bin_vendored::protoc_bin_path() {
-        std::env::set_var(
-            "PROTOC",
-            vendored_protoc_path,
-        );
+        std::env::set_var("PROTOC", vendored_protoc_path);
     } else {
-        eprintln!("Failed to find vendored protoc binary; trying to fall back to protoc in $PATH...");
+        eprintln!(
+            "Failed to find vendored protoc binary; trying to fall back to protoc in $PATH..."
+        );
     }
 
     prost_build::compile_protos(&["proto/glyphs.proto"], &["proto/"])?;
